c466af2b6985163a8a0bc6f57c2f4025c0e86dab
Kol1n13 <60143734+Kol1n13@users.noreply.github.com>
2025-12-07 21:21:58 +0300

    hw

---
+++ src/shared/modules/offer/rdo/offer.rdo.ts
@@ -0,0 +1,38 @@
+import { Expose, Type } from 'class-transformer';
+import { OfferCityRdo } from './offer-city.rdo'
+
+export class OfferRdo {
+  @Expose()
+  public id!: string;
+
+  @Expose()
+  public title!: string;
+
+  @Expose()
+  public type!: string;
+
+  @Expose()
+  public price!: number;
+
+  @Expose()
+  @Type(() => OfferCityRdo)
+  public city!: OfferCityRdo;
+
+  @Expose()
+  public previewImage!: string;
+
+  @Expose()
+  public isFavorite!: boolean;
+
+  @Expose()
+  public isPremium!: boolean;
+
+  @Expose()
+  public rating!: number;
+
+  @Expose()
+  public commentsCount!: number;
+
+  @Expose()
+  public postDate!: Date;
+}
---
+++ src/shared/modules/user/user.controller.ts
@@ -0,0 +1,65 @@
+import { inject, injectable } from 'inversify';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { BaseController, HttpError, HttpMethod } from '../../libs/rest/index.js';
+import { Logger } from '../../libs/logger/index.js';
+import { Component } from '../../types/index.js';
+import { UserService } from './user-service.interface.js';
+import { Config, RestSchema } from '../../libs/config/index.js';
+import { UserRdo } from './rdo/user.rdo.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
+import { plainToInstance } from 'class-transformer';
+
+@injectable()
+export class UserController extends BaseController {
+  constructor(
+    @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.UserService) private readonly userService: UserService,
+    @inject(Component.Config) private readonly configService: Config<RestSchema>,
+  ) {
+    super(logger);
+    this.logger.info('Register routes for UserController…');
+
+    this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: '/login', method: HttpMethod.Post, handler: this.login });
+    this.addRoute({ path: '/logout', method: HttpMethod.Post, handler: this.logout });
+  }
+
+  public async create(req: Request, res: Response): Promise<void> {
+    const body = req.body as CreateUserDto;
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» exists.`
+      );
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.created(res, plainToInstance(UserRdo, result, { excludeExtraneousValues: true }));
+  }
+
+  public async login(
+    req: Request,
+    _res: Response,
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(req.body.email);
+
+    if (! existsUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${req.body.email} not found.`
+      );
+    }
+
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented'
+    );
+  }
+
+  public async logout(_req: Request, res: Response): Promise<void> {
+    this.noContent(res, { message: 'Logged out successfully' });
+  }
+}
